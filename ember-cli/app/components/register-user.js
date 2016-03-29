124eabd2eaf2d6c12966174fb52183983a4dd3de
Andrzej Swaton <andrzej.swaton@gmail.com>
2016-03-29 10:08:45 +0200

    fix for communicate for dup key when registering, closes #24

--- ember-cli/app/components/register-user.js
+++ ember-cli/app/components/register-user.js
@@ -8,7 +8,19 @@ export default Ember.Component.extend({
     register() {
       const properties = this.getProperties(['identification','password']);
       this.get('onSave')(properties).catch((reason) => {
-      	this.set('errors', reason.errors);
+
+        var duplicateKeyError = (Ember.get(reason,'errors.0.detail') || "").indexOf("E11000") > -1 //"E11000 duplicate key error collection: test.users index: identification_1 dup key: { : "3" }"
+        let errors = [];
+        if(duplicateKeyError) {
+          errors = [{
+            message: `user with identification: ${properties.identification} already exists`
+          }];
+        }
+        else {
+          errors = reason.errors;
+        }
+        debugger;
+      	this.set('errors', errors);
       });
     }
   }
