17f6a4168b466df1de5ab72916e3a6acd681f135
Andrzej Swaton <andrzej.swaton@gmail.com>
2016-03-29 22:51:43 +0200

    time added, closes #21

--- ember-cli/app/components/expense-form.js
+++ ember-cli/app/components/expense-form.js
@@ -1,10 +1,20 @@
 import Ember from 'ember';
-
 export default Ember.Component.extend({
   date: new Date(),
+  didReceiveAttrs() {
+    this.set('time',this.get('time') || this.get('date'));
+  },
+  dateAndTime: Ember.computed('date','time',function(){
+    const date = this.get('date');
+    const time = this.get('time');
+    let ret = new Date(date);
+    ret.setHours(time.getHours(),time.getMinutes(),time.getSeconds());
+    return ret;
+  }),
   actions: {
     save() {
-      let data = this.getProperties(['amount','date','description','comment']);
+      let data = this.getProperties(['amount','description','comment']);
+      data.date =  this.get('dateAndTime');
       this.get('persist')(data);
     }
   }
