dfa3ab1ca8748d842749ad0c73401b18a1da26ff
Andrzej Swaton <andrzej.swaton@gmail.com>
2016-03-29 09:55:33 +0200

    fix for registretion - no token needed

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,15 +1,10 @@
 var express = require('express');
+var jsonapify = require('jsonapify');
+var _ = require('lodash');
 var myServerRouter = express.Router();
 var User = require('../models/user');
-var encryption = require('../utils/encryption');
-var async = require('async');
 var tokenUtils = require('../utils/token');
-var jsonapify = require('jsonapify');
 require('../resources/user');
-//TODO remove debug logger
-var logger = require('../utils/logger');
-var debugbody = logger.debugbody;
-var logErrors = logger.logErrors;
 
 function canRud() {
   return function(req,res,next) {
@@ -22,13 +17,16 @@ function canRud() {
   };
 }
 
+/**
+ * removes fields which cannot be modified if user does not have rights like admin or usermanager
+ */
 function modifyHackGuard() {
   return function(req,res,next) {
     var attrs = req.body.data.attributes;
-    if(!req.user.scopes.toggleAdmin) {
+    if(!_.get(req,'user.scopes.toggleAdmin')) {
       delete attrs.admin; //cant patch this so make sure its deleted. would be nice to have some info that someone was actually trying to hack if thats the case
     }
-    if(!req.user.scopes.toggleUsermanager) {
+    if(!_.get(req,'user.scopes.toggleUsermanager')) {
       delete attrs.usermanager; //cant patch this so make sure its deleted. would be nice to have some info that someone was actually trying to hack if thats the case
     }
     next();
@@ -38,7 +36,6 @@ function modifyHackGuard() {
 //TODO make sure that this uses SSL
 module.exports = function (app) {
   myServerRouter.route('/').post([
-    tokenUtils.expressJwtMiddleware(),
     modifyHackGuard(),
     jsonapify.create('User'),
     jsonapify.errorHandler('User')
@@ -54,7 +51,6 @@ module.exports = function (app) {
     tokenUtils.expressJwtMiddleware(),
     canRud(),  //this will work because req.params.id is undefined
     jsonapify.read(['User', jsonapify.param('id')]),
-    logErrors,
     jsonapify.errorHandler()
   ]);
 
